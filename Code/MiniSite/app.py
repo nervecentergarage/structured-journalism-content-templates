b227b386bf8cf83058a7d5a7499d288105a10cbb
Jerry Overton <jerryaoverton@yahoo.com>
2020-10-26 15:03:18 -0500

    added port as environment variable

--- Code/MiniSite/app.py
+++ Code/MiniSite/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ def index():
 def article():
     return render_template("article.html")
 
-#main
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.getenv('PORT'))
+    app.run(debug=True, port=port, host='0.0.0.0')
 
