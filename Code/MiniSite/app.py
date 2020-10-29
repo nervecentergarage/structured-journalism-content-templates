a2749570f75dad65ca9aafc741ae5e8fc374f107
Jerry Overton <jerryaoverton@yahoo.com>
2020-10-28 21:31:02 -0500

    display topic summaries based on dummy content

--- Code/MiniSite/app.py
+++ Code/MiniSite/app.py
@@ -10,7 +10,16 @@ def index():
         {'name': 'persona2', 'id': 'id2'},
         {'name': 'persona3', 'id': 'id3'}
     ]
-    return render_template("index.html", personas=personas)
+    topics = [
+        {'heading': 'topic1', 'summary': 'summary1', 'description': 'description1'},
+        {'heading': 'topic2', 'summary': 'summary2', 'description': 'description2'},
+        {'heading': 'topic3', 'summary': 'summary3', 'description': 'description3'},
+        {'heading': 'topic4', 'summary': 'summary4', 'description': 'description4'},
+        {'heading': 'topic5', 'summary': 'summary5', 'description': 'description5'},
+        {'heading': 'topic6', 'summary': 'summary6', 'description': 'description6'},
+        {'heading': 'topic7', 'summary': 'summary7', 'description': 'description7'}
+    ]
+    return render_template("index.html", personas=personas, topics=topics)
 
 
 @app.route("/article")
